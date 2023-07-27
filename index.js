49707a119b2afce2ea6fd677bff2f770e5327d23
Sarim Hasam <sarimhasanplace123@gmail.com>
2023-07-28 00:54:58 +0500

    Made the basic functionality work

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+let items = [];
+
 app.get("/", (req, res) => {
-	res.render("index.ejs");
+	res.render("index.ejs", {
+		listItems: items
+	});
+});
+
+app.post("/", (req, res) => {
+	// console.log(req.body.newItem);
+
+	let item = req.body.newItem;
+	items.push(item);
+	res.redirect("/");
 })
 
 app.listen(port, () => {
 	console.log(`The app is running on port ${port}`);
-})
+});
